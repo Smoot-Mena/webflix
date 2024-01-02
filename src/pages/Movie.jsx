fe18a6de7eb4b1336a364f8cd4f46ed463c2dda0
Smoot-Mena <Msmoot810@gmail.com>
2024-01-02 11:04:37 -0500

    Adds State and Location to navigate to movie page
    Can now console log info regarding selected movie on the movie page by using state and location.

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,6 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom';
 
 function Movie( {movie} ) {
+    const info = useLocation();
+    let navigate = useNavigate();
+    let [movieInfo, setMovieInfo] = useState(null);
+
+    movie = info.state.movie;
+    
+    const getMovieInfo = async () => {
+        try {
+            let response = await fetch(`https://www.omdbapi.com/?apikey=2231b390&t=${movie.Title}`);
+            let results = await response.json();
+            setMovieInfo(results);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    useEffect(() => {
+        getMovieInfo();
+    }, [])
+
+    console.log(movieInfo);
     
     return (
         <section>
