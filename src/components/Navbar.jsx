0050858264c17be5263e0dd839b801f8e0b37381
Smoot-Mena <Msmoot810@gmail.com>
2024-01-08 17:29:16 -0500

    Updates Navbar css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 function Navbar() {
     return (
         <section id='navigation'>
-            <Link to="/"><img id='webflix-logo' src="src/assets/webflix logo.png" alt="Webflix Logo" /></Link>
+            <Link to="/"><img id='webflix-logo' src="./src/assets/webflix logo.png" alt="Webflix Logo" /></Link>
             <Link to="/my-favorites/">MY FAVORITES</Link>
             <Link to="/search/">SEARCH MOVIES</Link>
             </section>
