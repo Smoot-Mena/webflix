de4937aa43e671190c839d6849f9cc798b6df376
Smoot-Mena <Msmoot810@gmail.com>
2024-12-13 11:05:04 -0500

    Renames the logo file

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from  "../assets/webflix-logo.png"
 
 function Navbar() {
     return (
         <section id='navigation'>
-            <Link to="/"><img id='webflix-logo' src="../assets/webflix logo.png" alt="Webflix Logo" /></Link>
+            <Link to="/"><img id='webflix-logo' src={logo} alt="Webflix Logo" /></Link>
             <Link to="/my-favorites/">MY FAVORITES</Link>
             <Link to="/search/">SEARCH MOVIES</Link>
             </section>
